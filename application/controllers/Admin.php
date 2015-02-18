6b75b5d74934bd5a2e9608b460ce661213f139ca
A00897783 <bd0421@gmail.com>
2015-02-18 14:12:31 -0800

    Part B done Signed-off-by:A00897783 <bd0421@gmail.com>

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -9,21 +9,37 @@
  */
 class Admin extends Application {
 
-    function __construct()
-    {
-	parent::__construct();
+    function __construct() {
+        parent::__construct();
+        $this->load->helper('formfields');
     }
 
     //-------------------------------------------------------------
     //  The normal pages
     //-------------------------------------------------------------
 
-   function index() {
-       
-    $this->data['title'] = 'Quotations Maintenance';
-    $this->data['quotes'] = $this->quotes->all();
-    $this->data['pagebody'] = 'admin_list'; 
-    $this->render();
+    function index() {
+
+        $this->data['title'] = 'Quotations Maintenance';
+        $this->data['quotes'] = $this->quotes->all();
+        $this->data['pagebody'] = 'admin_list';
+        $this->render();
+    }
+
+    function add() {
+        $quote = $this->quotes->create();
+        $this->present($quote);
+    }
+
+    // Present a quotation for adding/editing
+    function present($quote) {
+        $this->data['fid'] = makeTextField('ID#', 'id', $quote->id);
+        $this->data['fwho'] = makeTextField('Author', 'who', $quote->who);
+        $this->data['fmug'] = makeTextField('Picture', 'mug', $quote->mug);
+        $this->data['fwhat'] = makeTextArea('The Quote', 'what', $quote->what);
+        $this->data['pagebody'] = 'quote_edit';
+        $this->data['fsubmit'] = makeSubmitButton('Process Quote', "Click here to validate the quotation data", 'btn-success');
+        $this->render();
     }
 
 }
