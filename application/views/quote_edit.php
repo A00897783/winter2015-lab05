39c15219c8f00cb12332d0fe507a22435550e76d
A00897783 <bd0421@gmail.com>
2015-02-20 10:31:12 -0800

    Part B(added part) done Signed-off-by:A00897783 <bd0421@gmail.com>

--- application/views/quote_edit.php
+++ application/views/quote_edit.php
@@ -1,9 +1,10 @@
 <div class="row">
+  <div class="errors">{message}</div>
 <form action="/admin/confirm" method="post">    
     {fid}
     {fwho}
     {fmug}
     {fwhat}
-  </form>
     {fsubmit} 
+</form>
 </div>
